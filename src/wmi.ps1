6fbe501bc8e6058a5fe2c34608dfb673f8549914
meizhoubao <meizhoubao@users.noreply.github.com>
2015-07-10 20:29:20 +0800

    Delete wmi.ps1

--- src/wmi.ps1
+++
@@ -1,6 +0,0 @@
-# Language: powershell
-# Author: Leopard Li
-# Date: 2015-7-10
-
-# Before running the script,replace $mycomputername with your computer's name 
-gwmi Win32_OperatingSystem -ComputerName $mycomputername | Format-Table @{N='PSComputerName';E={$($_).CSName}},ServicePackMajorVersion,Version,@{N="BIOSSerial";E={$(gwmi Win32_BIOS -ComputerName $_.CSName).SerialNumber}}
